476acda603ab4f368b93aa7b953bfe8c8b1bdd1a
ENIOMARCOS33A <eniomlima@hotmail.com>
2024-06-06 17:13:57 +0000

    adcionada a função jogar novamente e finalização do projeto

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { trocarTema, verificarTema } from "./helpers/tema-helpers.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
+
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
@@ -14,7 +15,8 @@ botoesAssunto.forEach(botao => {
 })
 
 function selecionarAssunto (evento){
-    const assunto = evento.target.innerText
+    const classeBotao = evento.target.className
+    const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
     window.location.href = "./pages/quiz/quiz.html"        
 }
